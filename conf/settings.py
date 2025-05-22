3c4de52977cbaba026ed528ab75b5a3722a72fa6
adm1ngit <4dm1n217@gmail.com>
2025-05-22 15:33:09 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -130,7 +130,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-AUTH_USER_MODEL = "warranty_card.Admin"
 
 
 DATABASES = {
