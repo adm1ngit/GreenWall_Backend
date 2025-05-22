ba57bd1da253dd1ca58421e4e0773d5f93922b2a
adm1ngit <4dm1n217@gmail.com>
2025-05-22 13:17:51 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -70,7 +70,8 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
 }
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
 CORS_ALLOWED_ORIGINS = [
     "https://greenwalluz-4a1f8b314ff2.herokuapp.com",
     "http://localhost:8000",
@@ -80,9 +81,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://www.uzgreenwall.com",
-    "http://localhost:8000",
     "https://greenwalluz-4a1f8b314ff2.herokuapp.com",
-    "http://localhost:5173",
 ]
 
 
