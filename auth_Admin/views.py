f975961c8b345d2b4fc4d78f9ca64a6de5025330
adm1ngit <4dm1n217@gmail.com>
2025-05-31 15:08:29 +0500

    Update views.py

--- auth_Admin/views.py
+++ auth_Admin/views.py
@@ -1,7 +1,16 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import UserSubmission
 from .serializers import UserSubmissionSerializer
+from rest_framework.permissions import AllowAny
 
-class UserSubmissionCreateView(generics.CreateAPIView):
-    queryset = UserSubmission.objects.all()
-    serializer_class = UserSubmissionSerializer
+class UserSubmissionCreateView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSubmissionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Ma'lumot muvaffaqiyatli saqlandi!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
