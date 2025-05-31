a00910d577037faf77fd9c593e327c23ddfa8756
adm1ngit <4dm1n217@gmail.com>
2025-05-31 13:09:36 +0500

    ***

--- auth_Admin/admin.py
+++ auth_Admin/admin.py
@@ -3,6 +3,7 @@ from .models import UserSubmission
 
 # Register your models here.
 
+@admin.site.register(UserSubmission)
 class UserSubmissionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'phone')
     search_fields = ('name', 'phone')
